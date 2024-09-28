3a616987982283d7152731d8ded2a1bf26780353
hieuvolaptrinh <vndhieuak@gmail.com>
2024-09-28 18:33:30 +0700

    deploy thử api

---
+++ src/models/user.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  city: String,
+});
+
+const User = mongoose.model("User", userSchema);
+
+// module.exports = User;
+module.exports = { User, userSchema };
